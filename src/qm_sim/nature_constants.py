24a7dc423662a01ef3582fe9050d136e5e441cfa
Viljar Femoen <viljar.femoen@hotmail.no>
2023-01-21 14:25:47 +0100

    Add nature constants, remove example.py

---
+++ src/qm_sim/nature_constants.py
@@ -0,0 +1,12 @@
+"""
+
+Nature constants, in SI units
+
+"""
+
+e_0         =       1.602176634e-19         # [C]   Elementary charge, exact
+m_e         =       9.1093837015e-31        # [kg]  Electron mass
+h           =       6.62607015e-34          # [JS]  Planck constant, exact
+h_bar       =       1.054571817e-34         # [Js]  Reduced Planck constant  
+k_b         =       1.380649e-23            # [J/K] Boltzmann constant, exact
+𝜀_0         =       8.8541878128e-12        # [F/m] Vacuum permittivity
