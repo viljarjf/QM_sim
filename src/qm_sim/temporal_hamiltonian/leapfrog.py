8ba7c8704670195f470e27c78d9913b9a8b76db3
Viljar Femoen <viljar.femoen@hotmail.no>
2023-01-30 12:33:28 +0100

    Implement leapfrog iteration

--- src/qm_sim/temporal_hamiltonian/leapfrog.py
+++ src/qm_sim/temporal_hamiltonian/leapfrog.py
@@ -1,4 +1,5 @@
 from .base import BaseTemporalHamiltonian
+from ..nature_constants import h_bar
 
 import numpy as np
 
@@ -8,26 +9,40 @@ class Leapfrog(BaseTemporalHamiltonian):
     stable = True # conditionally stable, dt is chosen accordingly
     name = "leapfrog"
 
+    def iterate(self, t: float, dt_storage: float = None):
 
-    def _get_psi_0(self) -> np.ndarray:
-        """Calculate a initial state from eigenstates of `H0`.
-        This needs to be overridden to preserve the 2nd order accuracy
-
-        Returns:
-            np.ndarray: initial wave function
-        """
+        # Override initial condition to preserve 2nd order accuracy
         # psi_0 = psi_half - c*H_half*psi_half
         # psi_1 = psi_half + c*H_half*psi_half
         # H_half = H_0 + Vt_half
         # c = dt / (2i*hbar)
 
-        psi_half = super()._get_psi_0()
-        Vt_half = self.Vt(self.dt/2)
-        
-    
-           
-    def _get_dt(self) -> float:
-        pass
+        dt = self.dt
+        H0 = self.H0
+        Vt = self.Vt
 
-    def iterate(self, t: float, dt_storage: float = None):
-        pass
+        psi_half = super()._get_psi_0()
+        Vt_half = self.Vt(dt/2)
+        psi_0 = psi_half - dt / (2j*h_bar) * (self.H0 + Vt_half) @ psi_half
+        psi_1 = psi_half + dt / (2j*h_bar) * (self.H0 + Vt_half) @ psi_half
+
+        steps = 0
+        tn = 0
+        while tn < t:
+            steps += 1
+
+            # psi^n+1 = psi^n-1 + 2*dt*F^n
+            # F^n = 1/ihbar * H^n @ psi^n
+            # H^n = H0 + V^n
+
+            psi_2 = (H0 + Vt(tn)) @ (2*dt / (1j*h_bar) * psi_1) + psi_0
+
+            psi_0, psi_1 = psi_1, psi_2
+
+            tn += dt
+
+            # store data every `t_store` seconds
+            if tn // dt_storage > len(self.psi):
+                self.psi.append(psi_1)
+                self.t.append(tn)
+                self.V.append(Vt(tn))
