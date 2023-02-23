38be1f24f45c2efe790e76504f0e24423dfd8624
Viljar Femoen <viljar.femoen@hotmail.no>
2023-02-23 12:59:55 +0100

    Implement Crank-nicolosn

--- src/qm_sim/hamiltonian/temporal_derivative/crank_nicolson.py
+++ src/qm_sim/hamiltonian/temporal_derivative/crank_nicolson.py
@@ -1,10 +1,18 @@
 import numpy as np
 from scipy.sparse.linalg import spsolve
+from scipy.sparse import dia_matrix
 from tqdm import tqdm
 
 from ...nature_constants import h_bar
 from .base import BaseTemporalDerivative
 
+
+def I_plus_cH(a: complex, H: dia_matrix) -> dia_matrix:
+    out = H.copy()
+    out.data *= a
+    zero_ind = list(out.offsets).index(0)
+    out.data[zero_ind] += 1
+    return out
 
 class CrankNicolson(BaseTemporalDerivative):
     order = 2
@@ -15,26 +23,29 @@ class CrankNicolson(BaseTemporalDerivative):
     def iterate(self, t: float, dt_storage: float = None):
 
         dt = self.dt
-        H0 = self.H
+        H = self.H
         prefactor = dt/(2j * h_bar)
-        H = prefactor * H0
         Vt = self.Vt
         tn = 0
 
-        # Unity "matrix" (in a nice format for our )
-        I = np.ones(self.H0.N).flatten()
+        I = np.ones(H.N).flatten()
 
+        Hn = prefactor*H(tn)
         psi_n = self.psi_0
+
         pbar = tqdm(desc="Crank-Nicholson solver", total=t, disable=not self.H0.verbose)
         while tn < t:
 
-            # psi^n+1 = psi^n-1 + dt/2*(F^n + F^n-1)
+            # psi^n+1 = psi^n + dt/2*(F^n+1 + F^n)
             # F^n = 1/ihbar * H^n @ psi^n
-            # H^n = H0 + V^n
+            # => psi^n+1 = psi^n + dt/2ihbar*(H^n+1 @ psi^n+1 + H^n @ psi^n)
+            # (I - dt/2ihbar*(H^n+1) @ psi^n+1 = (I + dt/2ihbar*H^n) @ psi^n
 
-            Hn = prefactor * H0(t)
 
-            psi_n = spsolve(Hn - I, (H + I) @ psi_n)
+            psi_n = spsolve(
+                I_plus_cH(-prefactor, H(t + dt)), 
+                I_plus_cH(prefactor, H(t)) @ psi_n,
+                )
 
             tn += dt
             H = Hn
