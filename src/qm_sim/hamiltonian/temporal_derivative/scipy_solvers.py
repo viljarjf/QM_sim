96823988f824d282cd6912edcabb075d69638c3a
Viljar Femoen <viljar.femoen@hotmail.no>
2023-02-23 12:24:12 +0100

    Add tqdm logging to scipy solvers

--- src/qm_sim/hamiltonian/temporal_derivative/scipy_solvers.py
+++ src/qm_sim/hamiltonian/temporal_derivative/scipy_solvers.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.integrate import solve_ivp
+from tqdm import tqdm
 
 from ...nature_constants import h_bar
 from .base import BaseTemporalDerivative
@@ -11,15 +12,24 @@ class ScipySolver(BaseTemporalDerivative):
 
     def iterate(self, t_final: float, dt_storage: float = None) -> tuple[np.ndarray, np.ndarray]:
         if self.H.verbose:
-            print("Warning: verbose option not available for scipy temporal solvers")
         v_0 = self.v_0.astype(np.complex128).flatten()
+        
+        pbar = tqdm(desc=self.name + " solver", total=t_final, disable=not self.H.verbose)
+        pbar.bar_format = "{l_bar}{bar}| {n:#.02g}/{total:#.02g}"
+
+        # Precalculate the coefficient for negligible speedup
+        i_hbar_inv = 1 / (1j*h_bar)
+
+        def ode_fun(t, y):
+            pbar.update(t - pbar.n)
+            return (self.H(t) @ y) * i_hbar_inv
+
         sol = solve_ivp(
-            lambda t, y: (self.H(t) @ y) / (1j*h_bar), 
+            ode_fun, 
             [0, t_final], 
             v_0, 
             t_eval=np.arange(0, t_final, dt_storage),
             first_step=self.dt,
-            # dense_output=True,
             method=self.method,
             )
         t = sol.t
